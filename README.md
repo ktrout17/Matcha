e7553dfa02747094437f2f0583294972d63503e1
ktrout17 <ktrout@student.wethinkcode.co.za>
2020-01-13 16:18:44 +0200

    styling added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Matcha
+
+   Matcha is an online dating website created using Nodejs, Javascript and Bootstrap.
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ router.get('/login', (req, res, next) => {
 router.get('/register', (req, res) => res.render('register'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
 router.get('/resend', (req, res) => res.render('resend'));
+router.get('/editProfile', (req, res) => res.render('editProfile'));
 
 
 // Import Controllers
