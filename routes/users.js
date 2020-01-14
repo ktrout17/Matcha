b0cda0f35d97c31996b7c103cf2488e8e696181c
amaquena <alvin1king@gmail.com>
2020-01-14 11:17:39 +0200

    forgotten password completed

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,8 @@ router.get('/login', (req, res, next) => {
 router.get('/register', (req, res) => res.render('register'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
 router.get('/resend', (req, res) => res.render('resend'));
+router.get('/forgotPwd', (req, res) => res.render('forgotPwd'));
+router.get('/changePwd', (req, res) => res.render('changePwd'));
 
 
 // Import Controllers
@@ -27,6 +29,8 @@ router.get('/logout', UsersController.user_logout);
 
 // Handles user Token for email verification
 router.get('/confirmation/:userToken', UsersController.user_confirmation);
-router.post('/resend', UsersController.user_tokenResend)
+router.post('/resend', UsersController.user_tokenResend);
+router.post('/forgotPwd', UsersController.user_forgotPwd);
+router.post('/changePwd', UsersController.user_changePwd);
 
 module.exports = router;
