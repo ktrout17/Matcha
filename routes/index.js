7577cc69d43d1dca09b05ee706c2c3b200cafa30
amaquena <alvin1king@gmail.com>
2020-02-06 17:21:57 +0200

    views

--- routes/index.js
+++ routes/index.js
@@ -1,127 +1,219 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const Likes = require('../models/Likes');
-const Views = require('../models/Views');
-const { ensureAuthenticated } = require('../config/auth');
-const multer = require('multer');
-const storage = require('../config/fileStorage');
+const User = require("../models/User");
+const Likes = require("../models/Likes");
+const Views = require("../models/Views");
+const { ensureAuthenticated } = require("../config/auth");
+const multer = require("multer");
+const storage = require("../config/fileStorage");
 const upload = multer({
 	storage: storage.storage,
 	limits: storage.limits,
-	fileFilter: storage.fileFilter,
+	fileFilter: storage.fileFilter
 });
 
 // Render ejs view pages
-router.get('/', (req, res) => { res.render('welcome') });
+router.get("/", (req, res) => {
+	res.render("welcome");
+});
 
-router.get('/profiles/:id', async (req, res) => {
+function pro(req, res, next, id) {
+	return new Promise(resolve => {
+		resolve();
+	});
+}
+
+router.get("/profiles/:id", async (req, res, next) => {
 	const id = req.params.id;
 	let liked;
 
-	const newView = new Views({
-		_userId: req.user.id,
-		viewedId: id
-	})
-
-	newView.save(err => {
-		if (err) {
-			req.flash('error_msg', err.message);
-			console.log("err" + err);
-			res.status(500).redirect('/profiles/' + id);
-		}
-		User.findOneAndUpdate({ _id: id }, { $inc: { views: 1 } }, (err) => {
+	// await pro(req, res, next, id);
+	Views.count(
+		{ $and: [{ _userId: req.user.id }, { viewedId: id }] },
+		(err, count) => {
 			if (err) {
-				req.flash('error_msg', err.message);
+				req.flash("error_msg", err);
 				console.log("err" + err);
-				res.status(500).redirect('/profiles/' + id);
+				res.status(500).redirect("/profiles/" + id);
+				next();
 			}
-		})
-	})
+			if (!count) {
+				const newView = new Views({
+					_userId: req.user.id,
+					viewedId: id
+				});
 
-	await User.findById(id)
-		.exec()
-		.then(doc => {
-			Likes.findOne({ $and: [{ _userId: req.user.id }, { likedId: id }] }, (err) => {
-				if (err) {
-					req.flash('error_msg', err.message);
-					console.log("err" + err);
-					res.status(500).redirect('/profiles/' + id);
-				}
-			})
-				.exec()
-				.then((likeDoc) => {
-					Views.count({ viewedId: { $eq: id } }, (err) => {
-						if (err) {
-							req.flash('error_msg', err.message);
-							console.log("err" + err);
-							res.status(500).redirect('/profiles/' + id);
+				newView.save(err => {
+					if (err) {
+						req.flash("error_msg", err);
+						console.log("err: " + err);
+						res.status(500).redirect("/profiles/" + id);
+						next();
+					}
+					User.findOneAndUpdate(
+						{ _id: id },
+						{ $inc: { views: 1 } },
+						err => {
+							if (err) {
+								req.flash("error_msg", err);
+								console.log("err: " + err);
+								res.status(500).redirect("/profiles/" + id);
+								next();
+							}
 						}
-						if (likeDoc) liked = "unlike";
-						else liked = "like";
-					})
+					);
+				});
+			}
+			User.findById(id)
+				.exec()
+				.then(doc => {
+					Likes.findOne(
+						{ $and: [{ _userId: req.user.id }, { likedId: id }] },
+						err => {
+							if (err) {
+								req.flash("error_msg", err.message);
+								console.log("err" + err);
+								res.status(500).redirect("/profiles/" + id);
+								next();
+							}
+						}
+					)
 						.exec()
-						.then(result => {
-							res.render('profiles', {
-								"user": doc,
-								"liked": liked,
-								"viewed": result
+						.then(likeDoc => {
+							Views.count({ viewedId: { $eq: id } }, err => {
+								if (err) {
+									req.flash("error_msg", err.message);
+									console.log("err" + err);
+									res.status(500).redirect("/profiles/" + id);
+									next();
+								}
+								if (likeDoc) liked = "unlike";
+								else liked = "like";
 							})
+								.exec()
+								.then(result => {
+									res.render("profiles", {
+										user: doc,
+										liked: liked,
+										viewed: result
+									});
+								});
 						});
-				})
-		});
+				});
+		}
+	);
 });
 
-router.get('/chats', ensureAuthenticated, (req, res) => res.render('chats'));
+router.get("/chats", ensureAuthenticated, (req, res) => res.render("chats"));
 
-router.get('/suggestedMatchas', (req, res) => {
+router.get("/suggestedMatchas", (req, res) => {
 	User.find({
-		$and: [{
-			city: req.user.city
-		}, {
-			$or: [{
-				"interests.first": { $in: [req.user.interests['first'], req.user.interests['second'], req.user.interests['third'], req.user.interests['fourth'], req.user.interests['fifth']] },
-			}, {
-				"interests.second": { $in: [req.user.interests['first'], req.user.interests['second'], req.user.interests['third'], req.user.interests['fourth'], req.user.interests['fifth']] },
-			}, {
-				"interests.third": { $in: [req.user.interests['first'], req.user.interests['second'], req.user.interests['third'], req.user.interests['fourth'], req.user.interests['fifth']] },
-			}, {
-				"interests.fourth": { $in: [req.user.interests['first'], req.user.interests['second'], req.user.interests['third'], req.user.interests['fourth'], req.user.interests['fifth']] },
-			}, {
-				"interests.fifth": { $in: [req.user.interests['first'], req.user.interests['second'], req.user.interests['third'], req.user.interests['fourth'], req.user.interests['fifth']] },
-			}
-			]
-		}, {
-			$or: [{
-				$and: [{ gender: { $eq: req.user.sexPref } }, { gender: { $eq: 'male' } }]
+		$and: [
+			{
+				city: req.user.city
 			},
-			{ $and: [{ gender: { $eq: req.user.sexPref } }, { gender: { $eq: 'female' } }] },
-			{ gender2: { $eq: req.user.sexPref } }]
-		}, { _id: { $ne: req.user.id } }
+			{
+				$or: [
+					{
+						"interests.first": {
+							$in: [
+								req.user.interests["first"],
+								req.user.interests["second"],
+								req.user.interests["third"],
+								req.user.interests["fourth"],
+								req.user.interests["fifth"]
+							]
+						}
+					},
+					{
+						"interests.second": {
+							$in: [
+								req.user.interests["first"],
+								req.user.interests["second"],
+								req.user.interests["third"],
+								req.user.interests["fourth"],
+								req.user.interests["fifth"]
+							]
+						}
+					},
+					{
+						"interests.third": {
+							$in: [
+								req.user.interests["first"],
+								req.user.interests["second"],
+								req.user.interests["third"],
+								req.user.interests["fourth"],
+								req.user.interests["fifth"]
+							]
+						}
+					},
+					{
+						"interests.fourth": {
+							$in: [
+								req.user.interests["first"],
+								req.user.interests["second"],
+								req.user.interests["third"],
+								req.user.interests["fourth"],
+								req.user.interests["fifth"]
+							]
+						}
+					},
+					{
+						"interests.fifth": {
+							$in: [
+								req.user.interests["first"],
+								req.user.interests["second"],
+								req.user.interests["third"],
+								req.user.interests["fourth"],
+								req.user.interests["fifth"]
+							]
+						}
+					}
+				]
+			},
+			{
+				$or: [
+					{
+						$and: [
+							{ gender: { $eq: req.user.sexPref } },
+							{ gender: { $eq: "male" } }
+						]
+					},
+					{
+						$and: [
+							{ gender: { $eq: req.user.sexPref } },
+							{ gender: { $eq: "female" } }
+						]
+					},
+					{ gender2: { $eq: req.user.sexPref } }
+				]
+			},
+			{ _id: { $ne: req.user.id } }
 		]
-	}).sort({ fame: -1 })
-		.select('firstname lastname username profileImages.image1')
+	})
+		.sort({ fame: -1 })
+		.select("firstname lastname username profileImages.image1")
 		.exec()
 		.then(docs => {
-			res.status(200).render('suggestedMatchas', {
-				"users": docs.map(doc => {
+			res.status(200).render("suggestedMatchas", {
+				users: docs.map(doc => {
 					return {
 						firstname: doc.firstname,
 						lastname: doc.lastname,
 						username: doc.username,
 						profileImage: doc.profileImages.image1,
 						request: {
-							url: '/profiles/' + doc.id
+							url: "/profiles/" + doc.id
 						}
-					}
+					};
 				})
 			});
 		})
 		.catch();
 });
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-	res.render('dashboard', {
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+	res.render("dashboard", {
 		name: req.user.username,
 		pp: req.user.profileImages.image1,
 		province: req.user.province,
@@ -135,14 +227,22 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
 		interests: req.user.interests,
 		bio: req.user.bio,
 		likes: req.user.likes
-	}));
-router.get('/notifications', (req, res) => res.render('notifications'));
+	})
+);
+router.get("/notifications", (req, res) => res.render("notifications"));
 
 // Index Controller
 const IndexController = require("../controllers/index");
 
-router.post('/dashboard', (req, res, next) => { res.locals.upload = upload; next(); }, IndexController.index_dashboard);
-router.post('/profiles/:id', IndexController.index_profile);
-router.post('/suggestedMatchas', IndexController.index_advancedMathas);
+router.post(
+	"/dashboard",
+	(req, res, next) => {
+		res.locals.upload = upload;
+		next();
+	},
+	IndexController.index_dashboard
+);
+router.post("/profiles/:id", IndexController.index_profile);
+router.post("/suggestedMatchas", IndexController.index_advancedMathas);
 
 module.exports = router;
