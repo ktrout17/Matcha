64ed26080c872a5b86fb54ebef599ad49a2f2133
amaquena <alvin1king@gmail.com>
2020-02-13 10:28:36 +0200

    views on user page

--- models/Views.js
+++ models/Views.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ViewsSchema = new mongoose.Schema({
 	_userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-	viewedId: { type: mongoose.Schema.Types.ObjectId, required: true }
+	viewedId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }
 });
 
 const Views = mongoose.model('Views', ViewsSchema);
--- routes/index.js
+++ routes/index.js
@@ -17,98 +17,124 @@ router.get("/", (req, res) => {
 	res.render("welcome");
 });
 
-function pro(req, res, next, id) {
-	return new Promise(resolve => {
-		resolve();
-	});
-}
-
-router.get("/profiles/:id", async (req, res, next) => {
-	const id = req.params.id;
-	let liked;
-
-	// await pro(req, res, next, id);
-	Views.count(
+let find = (req, res, next, id) => {
+	Views.findOne(
 		{ $and: [{ _userId: req.user.id }, { viewedId: id }] },
-		(err, count) => {
+		(err, doc) => {
 			if (err) {
-				req.flash("error_msg", err);
-				console.log("err" + err);
-				res.status(500).redirect("/profiles/" + id);
-				next();
+				req.flash("error_msg", "1" + err.message);
+				return res.status(500).redirect("/profiles/" + id);
 			}
-			if (!count) {
+			if (!doc) {
 				const newView = new Views({
 					_userId: req.user.id,
 					viewedId: id
 				});
-
 				newView.save(err => {
 					if (err) {
-						req.flash("error_msg", err);
-						console.log("err: " + err);
-						res.status(500).redirect("/profiles/" + id);
-						next();
+						req.flash("error_msg", "2" + err.message);
+						return res.status(500).redirect("/profiles/" + id);
 					}
-					User.findOneAndUpdate(
-						{ _id: id },
-						{ $inc: { views: 1 } },
-						err => {
-							if (err) {
-								req.flash("error_msg", err);
-								console.log("err: " + err);
-								res.status(500).redirect("/profiles/" + id);
-								next();
-							}
-						}
-					);
 				});
+				User.findOneAndUpdate(
+					{ _id: id },
+					{ $inc: { views: 1 } },
+					err => {
+						if (err) {
+							req.flash("error_msg", "3" + err.message);
+							return res.status(500).redirect("/profiles/" + id);
+						} else {
+							next();
+						}
+					}
+				);
+			} else {
+				next();
 			}
-			User.findById(id)
-				.exec()
-				.then(doc => {
-					Likes.findOne(
-						{ $and: [{ _userId: req.user.id }, { likedId: id }] },
-						err => {
-							if (err) {
-								req.flash("error_msg", err.message);
-								console.log("err" + err);
-								res.status(500).redirect("/profiles/" + id);
-								next();
-							}
-						}
-					)
-						.exec()
-						.then(likeDoc => {
-							Views.count({ viewedId: { $eq: id } }, err => {
-								if (err) {
-									req.flash("error_msg", err.message);
-									console.log("err" + err);
-									res.status(500).redirect("/profiles/" + id);
-									next();
-								}
-								if (likeDoc) liked = "unlike";
-								else liked = "like";
-							})
-								.exec()
-								.then(result => {
-									res.render("profiles", {
-										user: doc,
-										liked: liked,
-										viewed: result
-									});
-								});
-						});
-				});
 		}
 	);
-});
+};
+
+router.get(
+	"/profiles/:id",
+	// (req, res, next) => {
+	// 	console.log("req.params.id: " + req.params.id)
+	// 	find(req, res, next, req.params.id);
+	// },
+	(req, res, next) => {
+		const id = req.params.id;
+		let liked;
+		// const userId = req.user.id;
+
+		// Views.findOne({ _userId: userId, viewedId: id }).then(result => {
+		// 	let visitor = false;
+
+		// 	if (!result) {
+		// 		visitor = true;
+		// 		const newView = new Views({
+		// 			_userId: req.user.id,
+		// 			viewedId: id
+		// 		});
+
+		// 		newView.save(err => {
+		// 			console.log(err);
+		// 		});
+
+		// 		User.findById( id )
+		// 			.exec()
+		// 			.then(user => {
+		// 				if (visitor) {
+		// 					user.views = user.views + 1;
+							// User.updateOne({ _id: id }, { $inc: { views: 1 } });
+							// return res.redirect("/profiles/" + id);
+							// return res.send("user" + user);
+					// 	}
+					// 	else {
+					// 		return res.redirect("/profiles/" + id);
+					// 	}
+					// });
+				// } else {
+				// return res.send();
+			// } else {
+			// 	res.send("done");
+				// return res.redirect("/profiles/" + id);
+			// }
+		// });
+
+		// 	if (id.match(/^[0-9a-fA-F]{24}$/)) {
+		User.findById(id)
+			.exec()
+			.then(doc => {
+		Likes.countDocuments(
+			{ $and: [{ _userId: req.user.id }, { likedId: id }] },
+			(err, likeDoc) => {
+				if (err) {
+					req.flash("error_msg", "4" + err.message);
+					return res.status(500).redirect("/profiles/" + id);
+					// next();
+				}
+				if (likeDoc > 0) liked = "unlike";
+				else liked = "like";
+
+			return res.render("profiles", {
+				user: doc,
+				liked: liked
+			});
+		});
+		});
+		// 	} else {
+		// 		res.send("not match");
+		// 		res.end();
+		// 	}
+	}
+);
 
 router.get("/chats", ensureAuthenticated, (req, res) => res.render("chats"));
 
 router.get("/suggestedMatchas", (req, res) => {
 	User.find({
-		$and: [
+		$or: [
+			// change $or back to $and for suggested searches
 			{
 				city: req.user.city
 			},
@@ -226,6 +252,7 @@ router.get("/dashboard", ensureAuthenticated, (req, res) =>
 		sexPref: req.user.sexPref,
 		interests: req.user.interests,
 		bio: req.user.bio,
+		views: req.user.views,
 		likes: req.user.likes
 	})
 );
