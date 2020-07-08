5bab8df2f25050292264660c0b92c18b5457e555
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-07-08 18:39:45 +0200

    UI fixes

--- public/socketHandler.js
+++ public/socketHandler.js
@@ -1,73 +1,74 @@
 $(function () {
-    const socket = io.connect()
+  const socket = io.connect();
 
-    var element = function (id) {
-        return document.getElementById(id);
-    }
+  var element = function (id) {
+    return document.getElementById(id);
+  };
 
-    // Get Elements
-    // get current logged in username
-    var user = element('userNameTag');
-    // Chat Page
-    var chatBox = element('chatBox');
-    var to = element('to');
-    var from = element('from');
-    var chatId = element('chatId');
-    var msgBox = element('msgBox');
-    // Login Page
-    var login = element("login")
-    var inputEmail = element('inputEmail');
-    // Profiles Page
-    var like_btn = element('like_btn');
-    var block_btn = element('block_btn');
-    var liked_username = element('liked_username');
-    var user_id = element('user_id');
-    var curr_userUsername = element('curr_userUsername');
-    var curr_userId = element('curr_userId');
+  // Get Elements
+  // get current logged in username
+  var user = element("userNameTag");
+  // Chat Page
+  var chatBox = element("chatBox");
+  var send = element("send");
+  var to = element("to");
+  var from = element("from");
+  var chatId = element("chatId");
+  var msgBox = element("msgBox");
+  // Login Page
+  var login = element("login");
+  var inputEmail = element("inputEmail");
+  // Profiles Page
+  var like_btn = element("like_btn");
+  var block_btn = element("block_btn");
+  var liked_username = element("liked_username");
+  var user_id = element("user_id");
+  var curr_userUsername = element("curr_userUsername");
+  var curr_userId = element("curr_userId");
 
-    // Notification Tab
-    var notify_message = element('notify-message');
-    var noti_btn =  element('noti_btn');
-    var thanks =  element('thanks');
-    var thanks2 =  element('thanks2');
-    let notifTag;
+  // Notification Tab
+  var notify_message = element("notify-message");
+  var noti_btn = element("noti_btn");
+  var thanks = element("thanks");
+  var thanks2 = element("thanks2");
+  let notifTag;
 
-    socket.on('connect', () => update());
-    function update() {
-        socket.emit('update', { user: user.value, id: socket.id });
-    }
+  socket.on("connect", () => update());
+  function update() {
+    socket.emit("update", { user: user.value, id: socket.id });
+  }
 
-    if (chatBox) {
-        chatBox.addEventListener('keydown', (event) => {
-          if (event.which === 13 && event.shiftKey == false) {
-            socket.emit('send_message', {
-              to: to.value,
-              from: from.value,
-              chatId: chatId.value,
-              message: chatBox.value,
-              time: getDateTime()
-            });
-            chatBox.value = '';
-            event.preventDefault();
-            } else {
-                console.log("no")
-            }
-        })
-    }
+  if (chatBox) {
+    chatBox.addEventListener("keydown", (event) => {
+      if (event.which === 13 && event.shiftKey == false) {
+        socket.emit("send_message", {
+          to: to.value,
+          from: from.value,
+          chatId: chatId.value,
+          message: chatBox.value,
+          time: getDateTime(),
+        });
+        chatBox.value = "";
+        event.preventDefault();
+      } else {
+        console.log("no");
+      }
+    });
+  }
 
-  // if (send) {
-  //   send.addEventListener("click", (event) => {
-  //     socket.emit("send_message", {
-  //       to: to.value,
-  //       from: from.value,
-  //       chatId: chatId.value,
-  //       message: chatBox.value,
-  //       time: getDateTime(),
-  //     });
-  //     chatBox.value = "";
-  //     event.preventDefault();
-  //   });
-  // }
+  if (send) {
+    send.addEventListener("click", (event) => {
+      socket.emit("send_message", {
+        to: to.value,
+        from: from.value,
+        chatId: chatId.value,
+        message: chatBox.value,
+        time: getDateTime(),
+      });
+      chatBox.value = "";
+      event.preventDefault();
+    });
+  }
 
   if (login) {
     login.addEventListener("click", (event) => {
@@ -92,7 +93,7 @@ $(function () {
       });
     });
   }
-  
+
   socket.on("notification", (data) => {
     let notifTag = document.createElement("p");
     if (data.match == 1) {
@@ -102,99 +103,103 @@ $(function () {
     }
   });
 
-    if (liked_username && curr_userUsername) {
-        if (liked_username.value && curr_userUsername.value) {
-            socket.emit('view', {
-                viewedUser: liked_username.value,
-                currUser: curr_userUsername.value,
-                viewedId: user_id.value,
-                currId: curr_userId.value
-            });
+  if (liked_username && curr_userUsername) {
+    if (liked_username.value && curr_userUsername.value) {
+      socket.emit("view", {
+        viewedUser: liked_username.value,
+        currUser: curr_userUsername.value,
+        viewedId: user_id.value,
+        currId: curr_userId.value,
+      });
+    }
+  }
+
+  if (noti_btn) {
+    socket.on("notification", (data) => {
+      noti_btn.classList.remove("btn-danger");
+      notifTag = document.createElement("p");
+      if (data.match == 1) {
+        notifTag.textContent = data.user + " " + data.msg + "!";
+      } else if (data.match == 2) {
+        notifTag.textContent = data.msg + " " + data.user + "!";
+      }
+      notify_message.appendChild(notifTag);
+    });
+  }
+
+  if (thanks && thanks2) {
+    thanks.addEventListener("click", () => {
+      noti_btn.classList.add("btn-danger");
+      while (notify_message.firstChild) {
+        notify_message.removeChild(notify_message.lastChild);
+      }
+    });
+    thanks2.addEventListener("click", () => {
+      noti_btn.classList.add("btn-danger");
+      while (notify_message.firstChild) {
+        notify_message.removeChild(notify_message.lastChild);
+      }
+    });
+  }
+
+  if (from) {
+    if (from.value) {
+      socket.on("recieve_message", (data) => {
+        var div1 = document.createElement("div");
+        var div2 = document.createElement("div");
+        var div3 = document.createElement("div");
+        var p1 = document.createElement("p");
+        var p2 = document.createElement("p");
+
+        if (from.value === data.from) {
+          div1.setAttribute("class", "media w-50 ml-auto mb-3");
+          div2.setAttribute("class", "media-body bg-white");
+          div3.setAttribute("class", "bg-danger rounded py-2 px-3 mb-2");
+          p1.setAttribute("class", "text-small mb-0 text-white");
+        } else if (from.value === data.to) {
+          div1.setAttribute("class", "media w-50 mb-3");
+          div2.setAttribute("class", "media-body ml-3");
+          div3.setAttribute("class", "bg-light rounded py-2 px-3 mb-2");
+          p1.setAttribute("class", "text-small mb-0 text-muted");
         }
-    }
+        p2.setAttribute("class", "small text-muted");
+        p1.textContent = data.msg;
+        p2.textContent = "from " + data.from + ": " + data.msgTime;
 
-    if (noti_btn){
-        socket.on('notification', (data) => {
-            noti_btn.classList.remove("btn-danger");
-            notifTag = document.createElement('p');
-            if (data.match == 1) {
-                notifTag.textContent = data.user + " " + data.msg + "!";
-            } else if (data.match == 2) {
-                notifTag.textContent = data.msg + " " + data.user + "!";
-            }
-            notify_message.appendChild(notifTag)
-        })
+        msgBox
+          .appendChild(div1)
+          .appendChild(div2)
+          .appendChild(div3)
+          .appendChild(p1);
+        div2.appendChild(p2);
+      });
     }
-
-    if (thanks && thanks2) {
-        thanks.addEventListener('click', () => {
-            noti_btn.classList.add("btn-danger");
-            while(notify_message.firstChild){
-                notify_message.removeChild(notify_message.lastChild);
-            }
-        })
-        thanks2.addEventListener('click', () => {
-            noti_btn.classList.add("btn-danger");
-            while(notify_message.firstChild){
-                notify_message.removeChild(notify_message.lastChild);
-            }
-        })
-    }
-
-    if (from) {
-        if (from.value) {
-            socket.on('recieve_message', (data) => {
-                var div1 = document.createElement('div');
-                var div2 = document.createElement('div');
-                var div3 = document.createElement('div');
-                var p1 = document.createElement('p');
-                var p2 = document.createElement('p');
-        
-                if (from.value === data.from) {
-                    div1.setAttribute('class', 'media w-50 ml-auto mb-3')
-                    div2.setAttribute('class', 'media-body bg-white')
-                    div3.setAttribute('class', 'bg-danger rounded py-2 px-3 mb-2')
-                    p1.setAttribute('class', 'text-small mb-0 text-white')
-                } else if (from.value === data.to) {
-                    div1.setAttribute('class', 'media w-50 mb-3')
-                    div2.setAttribute('class', 'media-body ml-3')
-                    div3.setAttribute('class', 'bg-light rounded py-2 px-3 mb-2')
-                    p1.setAttribute('class', 'text-small mb-0 text-muted')
-                }
-                p2.setAttribute('class', 'small text-muted');
-                p1.textContent = data.msg;
-                p2.textContent = 'from ' + data.from + ': ' + data.msgTime;
-        
-                msgBox.appendChild(div1).appendChild(div2).appendChild(div3).appendChild(p1)
-                div2.appendChild(p2)
-            })
-        }
-    }
+  }
 });
 
 function getDateTime() {
   var date = new Date();
 
-    var date = new Date();
+  var date = new Date();
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
 
-    var min = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
+  var min = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
 
-    var sec = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
+  var sec = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
 
-    var year = date.getFullYear();
+  var year = date.getFullYear();
 
-    var month = date.getMonth() + 1;
-    month = getMonth(parseInt((month < 10 ? "0" : "") + month));
+  var month = date.getMonth() + 1;
+  month = getMonth(parseInt((month < 10 ? "0" : "") + month));
 
-    var day = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
+  var day = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
 
-    return hour + ":" + min + " | " + month + " " + day;
+  return hour + ":" + min + " | " + month + " " + day;
 }
 
 function getMonth(m) {
