bae3afd3396f03f46023ba5786881b8b9840723a
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-07-10 11:44:45 +0200

    UI responsiveness on mobile devices

--- controllers/index.js
+++ controllers/index.js
@@ -380,30 +380,30 @@ exports.index_advancedMathas = (req, res) => {
         locQuery = {};
     }
     // gets the intresets that you are into and will filter it this way with your intresets been top
-    let count = 0;;
-    let userFirstChoice = 0;
-    let userSecondChoice = 0;
-    let userThirdChoice = 0;
-    let userFourthChoice = 0;
-    let userFirthChoice = 0;
-    let userIntresets = req.user.interests;
-    while (interests[count])
-    {
-      if (interests !== "undefined")
-      {
-        if (interests[count] == userIntresets.first)
-        userFirstChoice++;
-        else if (interests[count] == userIntresets.second)
-        userSecondChoice++;
-        else if (interests[count] == userIntresets.third)
-        userThirdChoice++;
-        else if (interests[count] == userIntresets.fourth)
-        userFourthChoice++;
-        else if (interests[count] == userIntresets.fifth)
-        userFirthChoice++;
-      }
-      count++;
-    }
+    // let count = 0;;
+    // let userFirstChoice = 0;
+    // let userSecondChoice = 0;
+    // let userThirdChoice = 0;
+    // let userFourthChoice = 0;
+    // let userFirthChoice = 0;
+    // let userIntresets = req.user.interests;
+    // while (interests[count])
+    // {
+    //   if (interests !== "undefined")
+    //   {
+    //     if (interests[count] == userIntresets.first)
+    //     userFirstChoice++;
+    //     else if (interests[count] == userIntresets.second)
+    //     userSecondChoice++;
+    //     else if (interests[count] == userIntresets.third)
+    //     userThirdChoice++;
+    //     else if (interests[count] == userIntresets.fourth)
+    //     userFourthChoice++;
+    //     else if (interests[count] == userIntresets.fifth)
+    //     userFirthChoice++;
+    //   }
+    //   count++;
+    // }
     User.find({
       $and: [
         // ageQuery,
